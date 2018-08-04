11d38507add15a8f912cef3d9d3e519af814bd14
OLskrain <Olskrain2@yandex.ru>
2018-08-04 11:21:28 +0300

    исправления ДЗ 2

--- app/src/main/java/com/geekbrains/weather/WeatherActivity.java
+++ app/src/main/java/com/geekbrains/weather/WeatherActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    private static final String TAG = "WeatherActivity";
+    private static final String TAG = String.valueOf(R.string.first_start);
     //методы жизненного цикла
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,80 +22,65 @@ public class WeatherActivity extends AppCompatActivity {
 
         String instanceState;
         if (savedInstanceState == null) {
-            instanceState =  String.valueOf(R.string.first_start);
-            System.out.println(instanceState);
+            instanceState =  getResources().getString(R.string.first_start);
         }
         else {
-            instanceState = String.valueOf(R.string.restart);
-            System.out.println(instanceState);
+            instanceState = getResources().getString(R.string.restart);
         }
-        Toast.makeText(getApplicationContext(), instanceState + " - " + R.string.onCreate, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onCreate");
+        Toast.makeText(getApplicationContext(), instanceState + " - " + getResources().getString(R.string.onCreate), Toast.LENGTH_SHORT).show();
+        Log.d(getResources().getString(R.string.TAG), getResources().getString(R.string.onCreate));
     }
 //методы жизненного цикла
     @Override
     protected void onStart() {
         super.onStart();
-        //Попытался забить весь текст в ресурсы. однако оказалось, что в Log.d(String.valueOf(R.string.TAG), String.valueOf(R.string.onStart);
-        //в Logcat прописывает int значения ресурсов R.string.onStart и R.string.TAG вместо текста. пока вернулся к старому варианту.
-        //в Toast.makeText(getApplicationContext(), R.string.onStart, Toast.LENGTH_SHORT).show(); и во всех метадах, кроме onCreate
-        //на экран эмулятора выводит "onStart" или другие значения соответственно. НО в методе onCreate выводит только int значения.
-        //Из-за этого не могу понять почему так по разному выводит. Предположение только одно. когда 2-ой параметр в Toast.makeText не
-        //составной, то он выводит текст, а когда составной, как в методе onCreate, то выводит int значения, вместо текста.
-        //отправляю как есть. Может подскажите как правильно организовать в таком случае код. или есть методы какие
-        Toast.makeText(getApplicationContext(), R.string.onStart, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStart");
+       toastAndLog(R.string.onStart);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle saveInstanceState){
         super.onRestoreInstanceState(saveInstanceState);
-        //тут специально оставил "Повторный запуск! - onRestoreInstanceState()" для проверки. если заменить, то тоже будет выводить int значения
-        Toast.makeText(getApplicationContext(), "Повторный запуск! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onRestoreInstanceState");
+        Toast.makeText(getApplicationContext(), getResources().getString(R.string.restart) + " - " + getResources().getString(R.string.onRestoreInstanceState), Toast.LENGTH_SHORT).show();
+        Log.d(getResources().getString(R.string.TAG), getResources().getString(R.string.onRestoreInstanceState));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //тут и дальше выводит R.string.onPause в виде текста а не int
-        Toast.makeText(getApplicationContext(), R.string.onPause, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onPause");
+       toastAndLog(R.string.onPause);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle saveInstanceState){
         super.onSaveInstanceState(saveInstanceState);
-        Toast.makeText(getApplicationContext(), R.string.onSaveInstanceState, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onSaveInstanceState");
+       toastAndLog(R.string.onSaveInstanceState);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), R.string.onResume, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onResume");
+      toastAndLog(R.string.onResume);
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(), R.string.onRestart, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onRestart");
-
+       toastAndLog(R.string.onRestart);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), R.string.onStop, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStop");
+        toastAndLog(R.string.onStop);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(), R.string.onDestroy, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDestroy");
+        toastAndLog(R.string.onDestroy);
+    }
+    public void toastAndLog(int resources){
+        Toast.makeText(getApplicationContext(), getResources().getString(resources), Toast.LENGTH_SHORT).show();
+        Log.d(getResources().getString(R.string.TAG), getResources().getString(resources));
     }
 }
